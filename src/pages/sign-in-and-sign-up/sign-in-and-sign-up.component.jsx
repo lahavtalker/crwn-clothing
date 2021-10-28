3d84696657a69a107315a037bc8d8295a09b6a10
Lahav Talker <lahavtalker@gmail.com>
2021-10-28 17:50:06 +0300

    added custom button component

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+import "./sign-in-and-sign-up.styles.scss";
+
+const SignInAndsignOut = () => {
+  return (
+    <div className="sign-in-and-sign-out">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndsignOut;
