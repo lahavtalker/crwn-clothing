789c4f4f30d01f6c1573e361e0427103facc9847
Lahav Talker <lahavtalker@gmail.com>
2021-11-12 12:42:05 +0200

    updating stripe integration and updating our favicon title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JueoGBBkksPNF6B6u6pui5l2eUX0tu3bBr0Z3CCi9bd6edAznn2Uo7hs7smOZ4ep9NEYjZDDNbvALgCUgvspulJ00hTcemHY8";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress=""
+      shippingAddress=""
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
